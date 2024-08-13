5c38bc49b2becfc741af1ea941326d2bfda3718c
akbRRksh <hahahadotcom98@gmail.com>
2024-08-13 22:24:24 +0700

    update index, css and add more styling

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 function calculateBMI() {
     const weight = parseFloat(document.getElementById('weight').value);
-    const height = parseFloat(document.getElementById('height').value) / 100; // convert to meters
+    const height = parseFloat(document.getElementById('height').value) / 100;
     const bmi = weight / (height * height);
     
     let category, description;
     if (bmi < 18.5) {
         category = "Kurang";
-        description = "Anda berada dalam kategori underweight atau berat badan kurang. Berat badan kurang bisa menjadi tanda bahwa Anda tidak makan cukup atau mungkin menunjukkan kondisi medis yang mendasarinya. Jika Anda berada dalam kategori ini, hubungi dokter Anda untuk evaluasi lebih lanjut.";
+        description = "Anda berada dalam kategori underweight atau berat badan kurang.";
     } else if (bmi >= 18.5 && bmi < 25) {
         category = "Normal";
-        description = "Anda berada dalam kategori normal. Komunitas medis merekomendasikan untuk menjaga berat badan Anda dalam rentang ini.";
+        description = "Anda berada dalam kategori normal.";
     } else if (bmi >= 25 && bmi < 30) {
         category = "Lebih";
-        description = "Anda berada dalam kategori overweight atau berat badan berlebih. Cara terbaik untuk menurunkan berat badan adalah dengan mengatur kalori makanan yang dikonsumsi dan berolahraga.";
+        description = "Anda berada dalam kategori overweight atau berat badan berlebih.";
     } else {
         category = "Obesitas";
-        description = "Anda berada dalam kategori obesitas. Jika BMI Anda berada dalam kategori ini maka Anda disarankan untuk menurunkan berat badan hingga batas normal.";
+        description = "Anda berada dalam kategori obesitas.";
     }
     
     document.getElementById('bmi-result').textContent = bmi.toFixed(1);
